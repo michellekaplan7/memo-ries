0436e6095f1eb9e7eb6025e35d3f23f2e37bbd75
Michelle Kaplan <kaplan.michelle7@gmail.com>
2020-05-28 10:44:45 -0600

    :crystal_ball: Add Landing component and import Landing into App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import './App.css';
+import Landing from '../Landing/Landing'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        HI
-      </header>
+      <Landing />
     </div>
   );
 }
