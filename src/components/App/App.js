1202fe7ddd6506bfe1f3f3eca830dd67bb9b47d9
John Watterson <john@breakthrewbrew.com>
2020-05-28 14:03:09 -0600

    :crystal_ball: Add Header component and render component in App based on path '/destinations'

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,20 +1,46 @@
 import React from 'react';
 import './App.css';
 import { Route, Switch } from "react-router-dom";
-import Landing from '../Landing/Landing'
+import { Link } from "react-router-dom";
+import Landing from '../Landing/Landing';
+import Header from '../Header/Header';
+import DestinationsContainer from '../DestinationsContainer/DestinationsContainer';
 
-function App() {
+const App = () => {
   return (
     <main className="App">
       <Switch>
-        <Route
-          exact
-          path="/"
-          render={() => <Landing />}
-        />
+          <Route
+            exact
+            path="/"
+            render={() => {
+              return (
+                <div>
+                  <Landing />
+                </div>
+              )
+            }}
+            />
+          <Route
+            exact
+            path = "/destinations"
+            render = {() => {
+              return (
+                <div>
+                  <Header />
+                  <p>destinations</p>
+                    <DestinationsContainer />
+                </div>
+              );
+            }}
+            />
       </Switch>
     </main>
   );
 }
 
 export default App;
+
+// <Link to = "/">
+//   <p>destinations</p>
+// </Link>
---
+++ src/components/DestinationsContainer/DestinationsContainer.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import './Header.css';
+import { Route, Switch } from "react-router-dom";
+import { Link } from "react-router-dom";
