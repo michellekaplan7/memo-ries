2d9768f8bd1653f1167fc32be14228a662b6f5a0
John Watterson <john@breakthrewbrew.com>
2020-05-28 16:00:36 -0600

    :crystal_ball: Add functionality that sets useState for the currently selected destination upon clicking on a destination button

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,15 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import { Route, Switch } from "react-router-dom";
 import { Link } from "react-router-dom";
 import Landing from '../Landing/Landing';
 import Header from '../Header/Header';
+import DestinationDetails from '../DestinationDetails/DestinationDetails';
 import DestinationsContainer from '../DestinationsContainer/DestinationsContainer';
 
 const App = () => {
+  const [selectedDestination, setSelectedDestination] = useState('');
   return (
     <main className="App">
       <Switch>
+          <Route
+            path = "/destinations/:destinationName"
+            render = {({match}) => {
+              return (
+                <div>
+                  <Header />
+                  <Link to = "/destinations">
+                    <p>destinations</p>
+                  </Link> > <p>{match.params.destinationName}</p>
+                <div className="destination-details-container">
+                  <DestinationDetails destination={ selectedDestination }/>
+
+                </div>
+                </div>
+                )
+              }}
+            />
+
+          <Route
+            exact
+            path = "/destinations"
+            render = {() => {
+              return (
+                <div>
+                  <Header />
+                  <p>destinations</p>
+                    <DestinationsContainer setSelectedDestination={setSelectedDestination}/>
+                </div>
+              );
+            }}
+            />
+
           <Route
             exact
             path="/"
@@ -21,19 +55,7 @@ const App = () => {
               )
             }}
             />
-          <Route
-            exact
-            path = "/destinations"
-            render = {() => {
-              return (
-                <div>
-                  <Header />
-                  <p>destinations</p>
-                    <DestinationsContainer />
-                </div>
-              );
-            }}
-            />
+
       </Switch>
     </main>
   );
