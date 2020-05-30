4a47a82c55080d7424161810195bf8ad4c4b6aad
Rachael Thomas <rnthomas001@gmail.com>
2020-05-30 12:15:33 -0600

    :crystal_ball: Refactor functional component for hooks to successfully record and display multiple audio clips

--- src/components/Recording/Recording.js
+++ src/components/Recording/Recording.js
@@ -1,121 +1,70 @@
-// // // // // // // // 
-//  Refactoring the recordings with hooks......
-// // // // // // // // 
+import React, { useState } from "react";
+import { Recorded } from "../Recorded/Recorded";
 
-// import React, { useEffect, useState } from 'react';
-// const App = (props) => {
-//   const [audio, setAudio] = useState({})
-//   const [rec, setRec] = useState({})
-//   useEffect( () => {
-//      getMediaRecordingApi()
-//   }, [])
-//   let audioChunks = []
-//   async function getMediaRecordingApi() {
-//     let audioPromise = await navigator.mediaDevices.getUserMedia({ audio: true, video: false})
-//     setAudio(audioPromise)
-//     let recording = await new MediaRecorder(audioPromise);
-//     setRec(recording)
-//     return audioPromise
-//   }
-//   function onSuccess() {
-//     rec.ondataavailable = (e) => {
-//     audioChunks.push(e.data);
-//     if (rec.state == "inactive"){
-//       let blob = new Blob(audioChunks, {type:'audio/mpeg-3'});
-//       const audioElement = document.getElementById('audio')
-//       audioElement.src = URL.createObjectURL(blob);
-//       audioElement.controls=true;
-//       audioElement.autoplay=false;
-//         }
-//       }
-//     }
-//     function startAudio() {
-//     console.log(rec)
-//     console.log(audio)
-//       // rec.recording.MediaRecorder.start()
-//     }
-//     function stopAudio() {
-//       // rec.recording.stop()
-//       // onSuccess()
-//     }
-//     return ( 
-//     <div className = "App" >
-//       <div className = "controls" >
-//         <button onClick={startAudio()} >Start microphone</button> 
-//         <button onClick={stopAudio()} >Stop microphone</button> 
-//       </div>
-//       <audio id="audio"></audio>
-//     </div>
-//     );
-// }
-// export default App;
+const Recording = () => {
+  let rec = {};
+  let audioChunks = [];
+  const [savedRecords, setSavedRecords] = useState([]);
 
+  const getMediaRecordingApi = async () => {
+    let audioPromise = await navigator.mediaDevices.getUserMedia({
+      audio: true,
+      video: false,
+    });
+    let recording = await new MediaRecorder(audioPromise);
+    rec = recording;
+    return audioPromise;
+  };
 
-
-// // // // // // // // 
-//  This is the working MediaRecorderApi that can render multiple Recordings.....
-// // // // // // // // 
-
-
-import React from 'react';
-import { Recorded } from '../Recorded/Recorded'
-
-class Recording extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      inputDestination: '',
-      savedRecords: [],
+  const onSuccess = () => {
+    rec.ondataavailable = (e) => {
+      audioChunks.push(e.data);
+      if (rec.state === "inactive") {
+        let blob = new Blob(audioChunks, { type: "audio/mpeg-3" });
+        let mediaRecording = {
+          src: URL.createObjectURL(blob),
+          controls: true,
+          autoPlay: false,
+          id: Math.floor(Math.random() * 100) + 1,
+        };
+        audioChunks = [];
+        setSavedRecords([...savedRecords, mediaRecording]);
+      }
     };
-    this.audio = this.audio()
-    this.audioChunks = []
-    this.rec = null
-  }
-  async audio() {
-    let audio = await navigator.mediaDevices.getUserMedia({ audio: true, video: false})
-    this.rec = await new MediaRecorder(audio);
-    return audio
-  }
-  onSuccess() {
-    this.rec.ondataavailable = (e) => {
-    this.audioChunks.push(e.data);
-    if (this.rec.state === "inactive"){
-      let blob = new Blob(this.audioChunks,{type:'audio/mpeg-3'});
-      let mediaRecording = {
-        src: URL.createObjectURL(blob),
-        controls: true,
-        autoPlay: false,
-      }
-      this.audioChunks = []
-      this.setState({ savedRecords: [...this.state.savedRecords, mediaRecording]})
-        }
-      }
-    }
-    startAudio = (e) => {
-      this.rec.start()
-    }
-    stopAudio = (e) => {
-      this.rec.stop()
-      this.onSuccess()
-    }
-    renderAllRecordings = () => {
-      let test = this.state.savedRecords.map( rec => {
-        return <Recorded src={rec.src} controls={rec.controls} autoPlay={rec.autoPlay} />
-      })
-     return test
-    }
-  render() {
-    return ( 
-    <div>
-      <div>
-        <button onClick={this.startAudio} >Start microphone</button> 
-        <button onClick={this.stopAudio} >Stop microphone</button> 
+  };
+
+  const startAudio = async () => {
+    await getMediaRecordingApi();
+    rec.start();
+  };
+
+  const stopAudio = () => {
+    rec.stop();
+    onSuccess();
+  };
+
+  const renderAllRecordings = () => {
+    let test = savedRecords.map((rec) => {
+      return (
+        <Recorded
+          key={rec.id}
+          src={rec.src}
+          controls={rec.controls}
+          autoPlay={rec.autoPlay}
+        />
+      );
+    });
+    return test;
+  };
+
+  return (
+    <div className="recording">
+      <div className="controls">
+        <button onClick={() => startAudio()}>Start microphone</button>
+        <button onClick={() => stopAudio()}>Stop microphone</button>
       </div>
-      <div>
-        {this.renderAllRecordings()}
-      </div>
+      <div>{renderAllRecordings()}</div>
     </div>
-    );
-  }
-}
+  );
+};
 export default Recording;
