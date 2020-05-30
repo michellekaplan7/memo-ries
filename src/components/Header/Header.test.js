f2d939f52810cf1524bf00f47f27c6715a64d396
Rachael Thomas <rnthomas001@gmail.com>
2020-05-30 15:57:57 -0600

    :bar_chart: Write and pass test for pathname updating when View All Memories button is clicked

--- src/components/Header/Header.test.js
+++ src/components/Header/Header.test.js
@@ -1,24 +1,36 @@
 import React from "react";
 import Header from "./Header";
 import { MemoryRouter } from "react-router-dom";
-import { render } from "@testing-library/react";
+import { Router } from "react-router";
+import { createMemoryHistory } from "history";
+import { render, fireEvent } from "@testing-library/react";
 import "@testing-library/jest-dom/";
 
 describe("Header", () => {
-    it('Should display the header when rendered', () => {
-        const { getByText } =  render(
-        <MemoryRouter>
-            <Header />
-        </MemoryRouter>);
+  it("Should display the header when rendered", () => {
+    const { getByText } = render(
+      <MemoryRouter>
+        <Header />
+      </MemoryRouter>
+    );
 
-        const appName = getByText('MEMO—ries')
-        const viewMemBtn = getByText('View All Memories')
+    const appName = getByText("MEMO—ries");
+    const viewMemBtn = getByText("View All Memories");
 
-        expect(appName).toBeInTheDocument();
-        expect(viewMemBtn).toBeInTheDocument();
-    })
-    
-    it('should update the pathname when the View All Memories button is clicked', () => {
-        
-    })
-})
+    expect(appName).toBeInTheDocument();
+    expect(viewMemBtn).toBeInTheDocument();
+  });
+
+  it("should update the pathname when the View All Memories button is clicked", () => {
+    const history = createMemoryHistory();
+    const { getByRole } = render(
+      <Router history={history}>
+        <Header />
+      </Router>
+    );
+
+    const memoryLink = getByRole("link", { name: "View All Memories" });
+    fireEvent.click(memoryLink);
+    expect(history.location.pathname).toBe("/memories");
+  });
+});
