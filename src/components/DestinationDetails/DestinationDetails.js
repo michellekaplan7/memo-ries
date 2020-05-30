0f57b2bbd16fea0ea76b70e24c57d9e29474addd
Rachael Thomas <rnthomas001@gmail.com>
2020-05-29 18:44:50 -0600

    :crystal_ball: Add SimpleImageSlider for image slideshow

--- src/components/DestinationDetails/DestinationDetails.js
+++ src/components/DestinationDetails/DestinationDetails.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import './DestinationDetails.css';
+import SimpleImageSlider from "react-simple-image-slider";
+
 
 const DestinationDetails = ({destination}) => {
+  const pictures = [
+		{ url: `/public/${destination.destination}_1.jpg` },
+		{ url: `/public/${destination.destination}_2.jpg` },
+		{ url: `/public/${destination.destination}_3.jpg` },
+  ];
+ 
   return(
-    <h2>
+    <div>
+      <h2>
       {destination.destinationFullName}
-    </h2>
+      </h2>
+      <div className="pictures">
+        <SimpleImageSlider width={600} height={400} images={pictures} />
+      </div>
+    </div>
   );
 }
 export default DestinationDetails;
