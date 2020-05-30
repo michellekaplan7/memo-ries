8e1ebaba57ab384624974e5e44d2ef90c92d0782
Rachael Thomas <rnthomas001@gmail.com>
2020-05-30 16:12:49 -0600

    :tada: Initial commit

---
+++ src/components/DestinationButton/DestinationButton.test.js
@@ -0,0 +1,11 @@
+import React from "react";
+import DestinationButton from "./DestinationButton";
+import { MemoryRouter } from "react-router-dom";
+import { render } from "@testing-library/react";
+import "@testing-library/jest-dom/";
+
+describe("DestinationButton", () => {
+    it('should display the name of a destination', () => {
+        
+    })
+})
