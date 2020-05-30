9376192722f696fa9b170ec513595f8156997377
Michelle Kaplan <kaplan.michelle7@gmail.com>
2020-05-30 15:45:30 -0600

    :crystal_ball: Add button element around link and formated document

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -1,14 +1,18 @@
-import React from 'react';
+import React from "react";
 import { Link } from "react-router-dom";
-import './Landing.css';
+import "./Landing.css";
 
 function Landing() {
-    return (
-        <div>
-            <h2>Welcome!</h2>
-            <Link to='/destinations'>Let the memo-ries begin!</Link>
-        </div>
-    )
+  return (
+    <div>
+      <h2>Welcome!</h2>
+      <button className="welcome-button">
+        <Link className="welcome-link" to="/destinations">
+          Let the memories begin!
+        </Link>
+      </button>
+    </div>
+  );
 }
 
-export default Landing
+export default Landing;
