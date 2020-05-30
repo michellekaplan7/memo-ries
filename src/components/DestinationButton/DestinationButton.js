5cf63a9266f01b61b54ba0fac5401a088a30e992
Rachael Thomas <rnthomas001@gmail.com>
2020-05-30 16:25:47 -0600

    :crystal_ball: Clean up file

--- src/components/DestinationButton/DestinationButton.js
+++ src/components/DestinationButton/DestinationButton.js
@@ -1,20 +1,17 @@
-import React from 'react';  
+import React from "react";
 import { Link } from "react-router-dom";
-import DestinationDetails from '../DestinationDetails/DestinationDetails';
-import Header from '../Header/Header';
 
-const DestinationButton = (props) => { 
-
-  return(
+const DestinationButton = (props) => {
+  return (
     <Link to={`/destinations/${props.destination.destination}`}>
       <button
-      id={props.destination.id}
-      onClick={() => props.setSelectedDestination(props.destination)}
+        id={props.destination.id}
+        onClick={() => props.setSelectedDestination(props.destination)}
       >
         {props.destination.destination}
-      </button> 
+      </button>
     </Link>
   );
- }
+};
 
 export default DestinationButton;
