ea63b9c1fc2b33b7b6f9dec247f0ca52989c4e3a
Michelle Kaplan <kaplan.michelle7@gmail.com>
2020-05-31 15:46:18 -0600

    :crystal_ball: Add skeleton of Memories component

---
+++ src/components/Memories/Memories.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Memories.css';
+import DestinationDetails from '../DestinationDetails/DestinationDetails'
+
+export const Memories = () => {
+
+    return (
+        <div>
+            <DestinationDetails />
+        </div>
+    )
+}
