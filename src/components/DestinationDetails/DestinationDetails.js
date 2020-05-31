6c95a299ae3c6c2795a10820c5232db5dad5a612
Quinn Elder <qre.turing@gmail.com>
2020-05-31 17:56:57 -0600

    crystal_ball: Refactoring

--- src/components/DestinationDetails/DestinationDetails.js
+++ src/components/DestinationDetails/DestinationDetails.js
@@ -3,23 +3,28 @@ import './DestinationDetails.css';
 import SimpleImageSlider from "react-simple-image-slider";
 import Recording from '../Recording/Recording'
 
-const DestinationDetails = ({destination}) => {
+const DestinationDetails = ({selectedDestination, destinations}) => {
+
   const pictures = [
-		{ url: `/images/${destination.destination}_1.jpg` },
-		{ url: `/images/${destination.destination}_2.jpg` },
-		{ url: `/images/${destination.destination}_3.jpg` },
+		{ url: `/images/${selectedDestination.destination}_1.jpg` },
+		{ url: `/images/${selectedDestination.destination}_2.jpg` },
+		{ url: `/images/${selectedDestination.destination}_3.jpg` },
   ];
- 
+  
   return(
     <div>
       <h2>
-      {destination.destinationFullName}
+      {selectedDestination.destinationFullName}
       </h2>
       <div className="pictures">
         <SimpleImageSlider width={600} height={400} images={pictures} />
       </div>
       <div>
-        <Recording key={destination.id}/>
+        <Recording 
+        key={selectedDestination.id}
+        destinations={destinations}
+        selectedDestination={selectedDestination}
+        />
       </div>
     </div>
   );
