dbeb8736dec9c31267a1f23f1fc68566b85f3f67
Rachael Thomas <rnthomas001@gmail.com>
2020-05-30 18:36:53 -0600

    :bar_chart: Write and pass test to make sure the container holds the proper number of buttons when rendered

---
+++ src/components/DestinationsContainer/DestinationsContainer.test.js
@@ -0,0 +1,18 @@
+import React from "react";
+import DestinationsContainer from "./DestinationsContainer";
+import { MemoryRouter } from "react-router-dom";
+import { render } from "@testing-library/react";
+import "@testing-library/jest-dom/";
+
+describe("DestinationContainer", () => {
+  it("holds the destination buttons", () => {
+    const { getAllByRole } = render(
+      <MemoryRouter>
+        <DestinationsContainer setSelectedDestination={""} />
+      </MemoryRouter>
+    );
+
+    const destinationButtons = getAllByRole('button');
+    expect(destinationButtons).toHaveLength(4);
+  });
+});
