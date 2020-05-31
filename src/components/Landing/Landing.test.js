30222a29f2a52f8125fd0ea0cd378c760f59f9d1
Michelle Kaplan <kaplan.michelle7@gmail.com>
2020-05-31 11:33:44 -0600

    :bar_chart: Add test to the Landing component

--- src/components/Landing/Landing.test.js
+++ src/components/Landing/Landing.test.js
@@ -1,22 +1,38 @@
 import React from "react";
 import Landing from "./Landing";
 import { MemoryRouter } from "react-router-dom";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import "@testing-library/jest-dom/";
+import { Router } from "react-router";
+import { createMemoryHistory } from "history";
 
 describe("Landing", () => {
-    it("should display the correct information", () => {
-        const router = (
-            <MemoryRouter>
-                <Landing />
-            </MemoryRouter>
-        );
+  it("should display the correct information", () => {
+    const router = (
+      <MemoryRouter>
+        <Landing />
+      </MemoryRouter>
+    );
 
-        const { getByText, getByRole } = render(router);
-        const welcomeMessage = getByText('Welcome!')
-        const link = getByRole("link", {name: 'Let the memo-ries begin!'})
+    const { getByText, getByRole, getByAltText } = render(router);
+    const memoriesLogo = getByAltText("memories logo");
+    const welcomeMessage = getByText("Welcome!");
+    const link = getByRole("link", { name: "Let the memories begin!" });
 
-        expect(welcomeMessage).toBeInTheDocument()
-        expect(link).toBeInTheDocument()
-    })
-})
+    expect(welcomeMessage).toBeInTheDocument();
+    expect(link).toBeInTheDocument();
+    expect(memoriesLogo).toBeInTheDocument();
+  });
+
+  it("should update the pathname upon clicking the button", () => {
+      const history = createMemoryHistory();
+      const { getByRole } = render(
+          <Router history={history}>
+              <Landing />
+          </Router>
+      )
+      const link = getByRole("link", { name: "Let the memories begin!" });
+      fireEvent.click(link);
+      expect(history.location.pathname).toBe("/destinations")
+  })
+});
