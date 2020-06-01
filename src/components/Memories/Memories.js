44e1b12c3127c03c7daf43908563639e98721ebe
Rachael Thomas <rnthomas001@gmail.com>
2020-06-01 16:07:30 -0600

    :crystal_ball: Add functionality to map over the recordings for each destination and render them under the memories component

--- src/components/Memories/Memories.js
+++ src/components/Memories/Memories.js
@@ -1,12 +1,32 @@
-import React from 'react';
-import './Memories.css';
-import DestinationDetails from '../DestinationDetails/DestinationDetails'
+import React from "react";
+import "./Memories.css";
+import { Recorded } from "../Recorded/Recorded";
 
-export const Memories = () => {
+export const Memories = ({ destinations }) => {
+  const displayMemories = (destination) => {
+    if (destination.recordings.length > 0) {
+      return destination.recordings.map((recording) => {
+        return (
+          <Recorded
+            key={recording.id}
+            src={recording.src}
+            controls={recording.controls}
+            autoPlay={recording.autoPlay}
+          />
+        );
+      });
+    }
+    return <p>You have no recordings for this location.</p>;
+  };
 
+  const destinationContainers = destinations.map((destination) => {
     return (
-        <div>
-            <DestinationDetails />
-        </div>
-    )
-}
+      <div className="desination-recordings-container">
+        <h3>{destination.destinationFullName} Memories:</h3>
+        {displayMemories(destination)}
+      </div>
+    );
+  });
+
+  return <div className="memories-container">{destinationContainers}</div>;
+};
