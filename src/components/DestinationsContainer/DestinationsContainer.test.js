1959a1baa3cb8977f5898eeb4f74d72a966d2df6
John Watterson <john@breakthrewbrew.com>
2020-06-01 17:32:41 -0600

    :bar_chart: Made sure that DestintionContainer was being passed the correct props in the testing suite

--- src/components/DestinationsContainer/DestinationsContainer.test.js
+++ src/components/DestinationsContainer/DestinationsContainer.test.js
@@ -4,11 +4,49 @@ import { MemoryRouter } from "react-router-dom";
 import { render } from "@testing-library/react";
 import "@testing-library/jest-dom/";
 
+let destinations, selectedDestination;
+
 describe("DestinationsContainer", () => {
   it("holds the destination buttons", () => {
+    destinations = [
+      {
+        destination: "Denver",
+        destinationFullName: "Denver",
+        id: 1,
+        recordings: [],
+      },
+      {
+        destination: "Aspen",
+        destinationFullName: "Aspen",
+        id: 2,
+        recordings: [],
+      },
+      {
+        destination: "FoCo",
+        destinationFullName: "Fort Collins",
+        id: 3,
+        recordings: [],
+      },
+      {
+        destination: "RMNP",
+        destinationFullName: "Rocky Mountain National Park",
+        id: 4,
+        recordings: [],
+      },
+    ];
+
+    selectedDestination = {
+          destination: "RMNP",
+          destinationFullName: "Rocky Mountain National Park",
+          id: 4,
+          recordings: [],
+        };
+
     const { getAllByRole } = render(
       <MemoryRouter>
-        <DestinationsContainer setSelectedDestination={""} />
+        <DestinationsContainer
+          setSelectedDestination={selectedDestination}
+          destinations={destinations}/>
       </MemoryRouter>
     );
 
