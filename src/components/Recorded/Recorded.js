aee7f8f300c22c0132b7a07b3f06e2b1810527b0
John Watterson <john@breakthrewbrew.com>
2020-06-01 17:10:42 -0600

    :crystal_ball: Add ARIA role to recorded component, so that it can be queried by unit test

--- src/components/Recorded/Recorded.js
+++ src/components/Recorded/Recorded.js
@@ -1,5 +1,5 @@
 import React from 'react'
 
 export const Recorded = ({ src, controls, autoPlay}) => {
-  return <audio src={src} controls={controls} autoPlay={autoPlay}></audio>
+  return <audio src={src} controls={controls} autoPlay={autoPlay} role={'audio'}></audio>
 }
