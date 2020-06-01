2d4fa256a20059e926e8715a054bfbd3b21e05a2
John Watterson <john@breakthrewbrew.com>
2020-06-01 17:21:40 -0600

    :bar_chart: Made sure that correct props were being passed into Recording component

--- src/components/Recording/Recording.test.js
+++ src/components/Recording/Recording.test.js
@@ -6,17 +6,11 @@ import { createMemoryHistory } from "history";
 import { render, fireEvent } from "@testing-library/react";
 import "@testing-library/jest-dom/";
 
-// TEST CHECKLIST
-
-
-// test that when stop microphone is clicked, stopAudio is called
-// test that when stop microphone is clicked, stopAudio is called, and onSuccess is called
-
 describe("Recording", () => {
   it("should display the buttons and number of recordings when rendered", () => {
     const { getByRole, getByText } = render(
       <MemoryRouter>
-        <Recording key={1} />
+        <Recording key={1} destinations={[]} selectedDestination={{}}/>
       </MemoryRouter>
     );
 
@@ -27,21 +21,5 @@ describe("Recording", () => {
     expect(startButton).toBeInTheDocument();
     expect(stopButton).toBeInTheDocument();
     expect(numRecordings).toBeInTheDocument();
-  });
-
-  it.skip("should invoke startAudio when the Start microphone button is clicked", () => {
-    const mockStartAudio = jest.fn();
-    const mockStopAudio = jest.fn();
-    const { getByRole } = render(
-      <MemoryRouter>
-        <Recording key={1} />
-      </MemoryRouter>
-    );
-
-    const startButton = getByRole("button", { name: "Start microphone" });
-    const stopButton = getByRole("button", { name: "Stop microphone" });
-    fireEvent.click(startButton);
-    fireEvent.click(stopButton);
-    expect(mockStartAudio).toHaveBeenCalledTimes(1);
   });
 });
